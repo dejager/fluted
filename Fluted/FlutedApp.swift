926041d468036aa24103884b37f0a5162cc03302
Nate de Jager <dejager@syng.la>
2024-01-16 19:10:20 -0800

    Adding proof of concept.

---
+++ Fluted/FlutedApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlutedApp.swift
+//  Fluted
+//
+//  Created by Nate de Jager on 2024-01-16.
+//
+
+import SwiftUI
+
+@main
+struct FlutedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }.windowStyle(.volumetric)
+    }
+}
